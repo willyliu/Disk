d4c3173156aa6f07ee3423499dff1a7477dd4e10
craigrouse <craig.rouse@gmail.com>
2019-05-29 09:11:39 +0100

    xcode project updates

---
+++ DiskExample/DiskTVExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiskTVExample
+//
+//  Created by Craig Rouse on 29/05/2019.
+//  Copyright © 2019 Saoud Rizwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- Sources/Disk.h
+++ Sources/Disk.h
@@ -19,13 +19,13 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-
-#import <UIKit/UIKit.h>
-
-//! Project version number for Disk.
-FOUNDATION_EXPORT double DiskVersionNumber;
-
-//! Project version string for Disk.
-FOUNDATION_EXPORT const unsigned char DiskVersionString[];
+//
+//#import <UIKit/UIKit.h>
+//
+////! Project version number for Disk.
+//FOUNDATION_EXPORT double DiskVersionNumber;
+//
+////! Project version string for Disk.
+//FOUNDATION_EXPORT const unsigned char DiskVersionString[];
 
 
